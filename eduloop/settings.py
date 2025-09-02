e8b70667289f9de2cd44052d31fd712b164c904f
roniahamed <mdroniahamed56@gmail.com>
2025-09-03 04:09:47 +0600

    conig .env files

--- eduloop/settings.py
+++ eduloop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&kn5g8n$dkwb*)9h6@6-v@q1th0e78vr*bpu4f$@c4-zjdi-ze'
+SECRET_KEY = config('SECRET_KEY', default='asdfasdfasdfasdf340897iuhkndkjry89ulksdnfjkgyauyufsdp[akjdfsy87yeodnfujeye9ru')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'eduloop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
