491ef3e7f827aaf1e93e531708f2ef3c4fe60544
roniahamed <mdroniahamed56@gmail.com>
2025-09-04 06:38:47 +0600

    update serializers

--- questions/serializers.py
+++ questions/serializers.py
@@ -73,10 +73,10 @@ class QuestionSerializer(serializers.ModelSerializer):
         return instance
     
 class QuestionDetailSerializer(serializers.ModelSerializer):
-    group = serializers.SlugRelatedField(slug_field='name', queryset=Group.objects.all(),read_only=True )
-    subject = serializers.SlugRelatedField(slug_field='name', queryset=Subject.objects.all(),read_only=True)
-    category = serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all ,read_only=True)
-    subcategory = serializers.SlugRelatedField(slug_field='name', queryset=SubCategory.objects.all(), allow_null=True, required=False,read_only=True)
+    group = serializers.SlugRelatedField(slug_field='name',read_only=True )
+    subject = serializers.SlugRelatedField(slug_field='name',read_only=True)
+    category = serializers.SlugRelatedField(slug_field='name' ,read_only=True)
+    subcategory = serializers.SlugRelatedField(slug_field='name', read_only=True)
     class Meta:
         model = Question
         fields = ['id', 'group', 'subject', 'category', 'sub_category', 'level', 'type', 'metadata', 'created_at', 'updated_at']
