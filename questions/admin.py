80b766de9390cd859a294b57e62d7940b23ebdd4
roniahamed <mdroniahamed56@gmail.com>
2025-09-06 05:59:34 +0600

    Admin page adding Pagination

--- questions/admin.py
+++ questions/admin.py
@@ -7,6 +7,7 @@ class GroupAdmin(ModelAdmin):
     list_display = ('id', 'name','description' ,'created_at')
     search_fields = ('name',)
     ordering = ('-created_at',)
+    list_per_page = 15
 
 
 @admin.register(Subject)
@@ -14,18 +15,20 @@ class SubjectAdmin(ModelAdmin):
     list_display = ('id', 'name','description', 'group', 'created_at')
     search_fields = ('name', 'group__name')
     ordering = ('-created_at',)
-
+    list_per_page = 15
 @admin.register(Category)
 class CategoryAdmin(ModelAdmin):
     list_display = ('id', 'name', 'subject','group','created_at')
     search_fields = ('name', 'subject__name')
     ordering = ('-created_at',)
+    list_per_page = 15
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(ModelAdmin):
     list_display = ('id', 'name', 'category','subject','group', 'created_at')
     search_fields = ('name', 'category__name')
     ordering = ('-created_at',)
+    list_per_page = 15
 
 @admin.register(Question)
 class QuestionAdmin(ModelAdmin):
@@ -33,3 +36,4 @@ class QuestionAdmin(ModelAdmin):
     search_fields = ('type', 'level', 'group__name', 'subject__name', 'category__name', 'subcategory__name')
     list_filter = ('type', 'level', 'group', 'subject', 'category', 'subcategory')
     ordering = ('-created_at',)
+    list_per_page = 15
