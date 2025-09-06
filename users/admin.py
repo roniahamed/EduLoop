7d1ddb0e6f88611ff4e9dd250275c192a55c8d76
roniahamed <mdroniahamed56@gmail.com>
2025-09-06 06:29:36 +0600

    Access Token

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import AccessToken
 
-# Register your models here.
+
+@admin.register(AccessToken)
+class AccessTokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'description', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('key', 'description')
+    readonly_fields = ('key', 'created_at')
+    ordering = ('-created_at',)
