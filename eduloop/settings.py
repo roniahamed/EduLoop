877ad36515584c5d3196fcf4d1fc89be8d9ccd7b
roniahamed <mdroniahamed56@gmail.com>
2025-09-07 01:52:45 +0600

    permission setting

--- eduloop/settings.py
+++ eduloop/settings.py
@@ -134,3 +134,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST Framework Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'users.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        # 'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}   
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import ValidateAccessTokenView
 
 urlpatterns = [
-    path('token-verify', ValidateAccessTokenView.as_view(), name='validate-token'),
+    path('token-verify/', ValidateAccessTokenView.as_view(), name='validate-token'),
+    path('',include('rest_framework.urls')),
 ]
