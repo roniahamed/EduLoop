2688ad3c966907bec00ef4fe490be85487a60e20
roniahamed <mdroniahamed56@gmail.com>
2025-09-07 06:00:45 +0600

    updated

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('token-verify/', ValidateAccessTokenView.as_view(), name='validate-token'),
-    path('',include('rest_framework.urls')),
+    # path('',include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
