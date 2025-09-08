a31ca178e23a5c4626024a596cddeeaa490d9dae
roniahamed <mdroniahamed56@gmail.com>
2025-09-08 06:25:44 +0600

    update requirements.txt

---
+++ gunicorn.conf.py
@@ -0,0 +1,27 @@
+# gunicorn.conf.py
+
+# Socket to bind to.
+# '0.0.0.0:8000' মানে হলো সব নেটওয়ার্ক ইন্টারফেসের 8000 পোর্টে সার্ভার চলবে।
+bind = "0.0.0.0:8008"
+
+# Number of worker processes.
+# সাধারণত CPU কোরের সংখ্যার দ্বিগুণ + ১ রাখা হয়।
+# import multiprocessing
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 3  # আপাতত আমরা ৩ দিয়ে শুরু করতে পারি
+
+# The user to run as.
+# user = "www-data"  # প্রোডাকশন সার্ভারে নির্দিষ্ট ইউজার সেট করা ভালো অভ্যাস
+
+# The group to run as.
+# group = "www-data" # প্রোডাকশন সার্ভারে নির্দিষ্ট গ্রুপ সেট করা ভালো অভ্যাস
+
+# Logging
+# Access log - records incoming requests
+accesslog = "gunicorn_access.log"
+
+# Error log - records Gunicorn errors
+errorlog = "gunicorn_error.log"
+
+# Log level
+loglevel = "info"
