8907739622d67c1047f4eff9201cedd0779076f5
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-23 03:17:25 +0600

    feat: add GenerateAccessTokenView to user URL patterns

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ValidateAccessTokenView
+from .views import ValidateAccessTokenView, GenerateAccessTokenView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('token-verify/', ValidateAccessTokenView.as_view(), name='validate-token'),
     # path('',include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('token-generate/', GenerateAccessTokenView.as_view(), name='generate-token'),
 ]
