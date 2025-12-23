33e2c4235b0b908c7e522efef86642cc5caf845d
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-24 05:00:27 +0600

    feat: implement UserSerializer for user registration and validation

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'confirm_password']
+    
+    def validate(self, data):
+        if data['password'] != data.pop('confirm_password'):
+            raise serializers.ValidationError("Die Passwörter stimmen nicht überein.")
+        
+        if 'password' not in data or len(data['password']) < 8:
+            raise serializers.ValidationError("Das Passwort muss mindestens 8 Zeichen lang sein.")
+        return data
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data, password=password)
+        return user
+    
