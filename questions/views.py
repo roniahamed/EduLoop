f48c6ffb3825c3c8f13a16f5f857a190ec2577de
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-26 04:26:33 +0600

    fix: remove duplicate import of RetrieveUpdateDestroyAPIView in views.py

--- questions/views.py
+++ questions/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 from .permissions import IsAdminOrReadOnly
 from django.contrib.sessions.backends.db import SessionStore
